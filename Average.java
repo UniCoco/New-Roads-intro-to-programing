9e6a88e04528384fa2170114c667504fc5e9d1f2
ldrane <ldrane@newroads.org>
2016-09-07 12:31:15 -0400

    added Average class

--- Average.java
+++ Average.java
@@ -1,17 +1,14 @@
-//rember that when starting a java file the file must be named the same as your start aka "public class average.rember
-
-//I would name this file like if i had public class dog, i would call the file dog.java
 import java.util.Scanner;
 
 public class Average{
   
    public static void main(String[] args){
     
-    Scanenr input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
      
      System.out.println("Enter 3 numbers:  ");
      
-      double num1 = input.nextdouble();
+      double num1 = input.nextDouble();
        double num2 = input.nextDouble();
        double num3 = input.nextDouble();
        
@@ -19,6 +16,10 @@ public class Average{
        
        System.out.println("this is the average" + average);
     
+     //rember that when starting a java file the file must be named the same as your start aka "public class average.rember
+
+    //I would name this file like if i had public class dog, i would call the file dog.java
+
      
      //when error comes up '' what ever is in the quotes is what u need to do.  it also tells u the line like java7 
      
