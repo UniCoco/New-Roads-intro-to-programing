e5cedbce9752fc394253cad4b38e605a55ac79e0
ldrane <ldrane@newroads.org>
2016-09-28 12:04:45 -0400

    revire

---
+++ TestDataTypes.java
@@ -0,0 +1,29 @@
+public class TestDataTypes{
+//name must follower rules, can't start with a number, and for a class name it should be caps 
+  
+  public static void main(String[] args){
+  //this is the the sintacts for main method 
+    //int=whole number aka intager
+    int x = 18  ;
+    //int is the data type, x could be something like age so int age = 18 varible name.  
+    //double = decimal number 
+    double x2 = 2.;
+    //write a program that holds how much money is in account 
+    double balance = 1000.38;
+    double portion = 0.5;
+    
+    //String is a nother data type string is a string of characters 
+    //string is always caplized 
+    String x3 = "cat";
+    String y = "adasdaasfas";
+    String z = "32";
+      
+      String word = x3 + y; //output ---> catadasdaasfas
+    
+      //char = a single character 
+      char xx = 'x';
+      char yy = '3';
+      char zz = '2';
+  }
+    
+}
